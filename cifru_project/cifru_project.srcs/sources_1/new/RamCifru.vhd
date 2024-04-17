ab3f50307384b62aa3dabf8740924bbbdca8b14e
KOTerra <82443072+KOTerra@users.noreply.github.com>
2024-04-17 20:20:06 +0300

    close #5

--- cifru_project/cifru_project.srcs/sources_1/new/RamCifru.vhd
+++ cifru_project/cifru_project.srcs/sources_1/new/RamCifru.vhd
@@ -1,15 +1,28 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
 entity RamCifru is
-    Port ( a_ram : in BIT_VECTOR (1 downto 0); --Adresele
-           cs_ram : in STD_LOGIC;  -- Semnal selec?ie cip, „Chip Select"
-           we : in STD_LOGIC; -- Semnal validare scriere, „Write Enable"
-           d_ram : inout BIT_VECTOR (3 downto 0));  -- Ie?iri de date (bidirectional)
+    port (
+        address : in std_logic_vector (1 downto 0); 
+        writeEnable : in std_logic; 
+        data : inout std_logic_vector (3 downto 0)); 
 end RamCifru;
 
 architecture Behavioral of RamCifru is
-
+    type RamArray is array(0 to 2) of std_logic_vector(3 downto 0);
+    signal memory : RamArray := (
+        x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00",
+        x"00", x"00", x"00", x"00"
+    );
 begin
-
-
+    process (address, writeEnable, data)
+    begin
+        if writeEnable = '0' then --read
+            memory(to_integer(unsigned(address))) <= data;
+        else --write
+            data <= memory(to_integer(unsigned(address)));
+        end if;
+    end process;
 end Behavioral;
